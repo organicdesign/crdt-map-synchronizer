cbd654485b98c60ccc08905d52ba79850ae26e16
saul <saul@organicdesign.co.nz>
2023-02-06 09:20:59 +1300

    Add basic test.

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,3 +1,6 @@
-describe("Empty test", () => {
-	it("runs", () => {});
-});
+import { createSyncTest, mockCRDTMap } from "../../crdt-tests/src/index.js";
+import { createCRDTMapSynchronizer } from "../src/index.js";
+
+const mockMap = () => mockCRDTMap(createCRDTMapSynchronizer());
+
+createSyncTest(mockMap, (crdt, index) => crdt.action(index));
